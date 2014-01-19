683fc530ba75f8932d31efe4533751bad5530020
Jan Lelis <mail@janlelis.de>
2014-01-19 20:57:57 +0100

    fail faster for repl method

--- lib/print_debug/repl.rb
+++ lib/print_debug/repl.rb
@@ -4,16 +4,13 @@ require 'binding.repl'
 begin
   require 'binding_of_caller'
 rescue LoadError
+  warn 'Using PrintDebug#repl requires the "binding_of_caller" gem. Please install it or add it to your Gemfile!'
 end
 
 module PrintDebug
   private
 
   def repl
-    if defined?(BindingOfCaller)
-      binding.of_caller(1).repl.auto
-    else
-      raise ArgumentError, 'Using Inspection#repl requires the "binding_of_caller" gem. Please install it or add it to your Gemfile!'
-    end
+    binding.of_caller(1).repl.auto
   end
 end
