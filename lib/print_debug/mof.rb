e38d31be24919055a42219072853273df3680cc3
Jan Lelis <mail@janlelis.de>
2014-01-19 19:56:35 +0100

    improve and colorfy method list

--- lib/print_debug/mof.rb
+++ lib/print_debug/mof.rb
@@ -3,19 +3,30 @@ require 'print_debug'
 module PrintDebug
   private
 
-  def mof(obj, depth = 1)
+  def mof(obj, depth = nil, grep_for = //)
+    grep_for = Regexp.new(grep_for)
+    puts Paint["###", :red, :bold]
+
+    eigen_methods = obj.singleton_methods.grep(grep_for)
+    if eigen_methods.empty?
+      puts Paint['Eigenclass', :yellow]
+    else
+      puts Paint['Eigenclass', :green, :underline], eigen_methods.map(&:to_s)*'  '
+    end
+    puts
+
     if obj.is_a? Module
       klass, method_function = obj, :public_methods
     else
       klass, method_function = obj.class, :public_instance_methods
     end
 
-    eigen = obj.singleton_methods
-    puts '= Eigenclass', obj.singleton_methods.map(&:to_s)*'  ' unless eigen.empty?
-
-    depth.times{ |level|
+    (depth || klass.ancestors.size).times{ |level|
       if cur = klass.ancestors[level]
-        puts "= #{cur}", cur.send(method_function, false).map(&:to_s)*'  '
+        level_methods = cur.send(method_function, false).grep(grep_for)
+        colors = level_methods.empty? ? [:yellow] : [:green, :underline]
+        puts Paint["#{cur}", *colors], level_methods.map(&:to_s)*'  '
+        puts unless level_methods.empty?
       end
     }
 
